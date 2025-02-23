558cc60f6248b86476197cdc1223235640d5d19a
sachimbo15 <shogunato96@gmail.com>
2025-02-22 20:03:42 -0800

    Added Division function

--- app.py
+++ app.py
@@ -26,6 +26,16 @@ def multiply():
         return jsonify({"error": "Missing parameters"}), 400
     return jsonify({"result": a * b})
 
+@app.route('/divide')
+def divide():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a is None or b is None:
+        return jsonify({"error": "Missing Parameters"}), 400
+    elif b == 0:
+        return jsonify({"error": "Cannot divide by Zero"}), 400 
+    return jsonify({"result": a / b})
+
 if __name__ == '__main__':
     app.run(debug=True)
 
